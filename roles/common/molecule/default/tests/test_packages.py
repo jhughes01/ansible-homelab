7b6d55b423e9eabba91f2bab430240c0a6235c2b
Jackson Mark Hughes <jxhughes95@gmail.com>
2018-07-27 22:00:35 +0100

    Added tests to check packages defined in packages task are installed & added pycache to gitignore.

--- roles/common/molecule/default/tests/test_packages.py
+++ roles/common/molecule/default/tests/test_packages.py
@@ -5,8 +5,14 @@ import testinfra.utils.ansible_runner
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
-def test_git_installed(host)
+
+def test_git_installed(host):
     git = host.package("git")
 
-    assert git.isinstalled
-    
+    assert git.is_installed
+
+
+def test_bindutils_installed(host):
+    bindutils = host.package("bind-utils")
+
+    assert bindutils.is_installed
